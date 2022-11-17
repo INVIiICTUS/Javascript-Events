ddd3a9e1d1af67ac9a3828d24737fe4dafaad3f7
INVIiICTUS <j.enriquealorenzo@gmail.com>
2022-11-17 19:13:23 +0000

    Finalizados

--- exercises/03-sum-values/index.js
+++ exercises/03-sum-values/index.js
@@ -1,9 +1,14 @@
 // adding the function to the window makes it globally available
-window.calculateSumListener = function() {
-	//Return the value of the input #firstNumber
-	var stringA = document.getElementById("firstNumber").value;
-	//Return the value of the input #secondNumber
-	var stringB = document.getElementById("secondNumber").value;
-
-	//your code goes here
+window.calculateSumListener = function calculateSumListener() {
+    //Return the value of the input #firstNumber
+    var stringA = document.getElementById("firstNumber").value;
+    //Return the value of the input #secondNumber
+    var stringB = document.getElementById("secondNumber").value;
+    //your code goes here
+    let num1 = parseInt(stringA);
+    let num2 = parseInt(stringB);
+    let result = num1 + num2;
+    console.log(result);
+    //document.getElementById(resultNumber).innerHTML = "hola";
+    document.getElementById("resultNumber").value = result;
 };
--- exercises/06-add-listener-with-js/index.js
+++ exercises/06-add-listener-with-js/index.js
@@ -1,6 +1,10 @@
 window.onload = function myLoadFunction() {
-	alert("The website just finished loading!");
-	//some code here
+    alert("The website just finished loading!");
+    //some code here
 };
 
 //the listener function here
+
+window.wooHoo = function wooHoo() {
+    alert("woohoo!");
+}
